4653d76e9357b119216bd3b5e31c34a02d561012
TSAR10 <sureka.tushar1@gmail.com>
2020-07-12 06:51:35 +0530

    beautified

--- home.js
+++ home.js
@@ -1,11 +1,11 @@
 function zindex(){
 	var pos = document.getElementsByClassName("menubar")[0];
 	var pos1 = window.getComputedStyle(pos, null).getPropertyValue("left");
-	if(pos1 < "0px")
+	if(pos1 == "0px")
 	{
 		document.getElementsByClassName("navbar")[0].style.zIndex="5000";
 	}
-	else if(pos1 == "0px")
+	else if(pos1 > "0px")
 	{
 		document.getElementsByClassName("navbar")[0].style.zIndex="100";
 	}
